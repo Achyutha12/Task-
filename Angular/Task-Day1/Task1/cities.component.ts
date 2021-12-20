fd0b738fcde31e8c296a68f23b1a9944d128de72
Achyutha12 <96127510+Achyutha12@users.noreply.github.com>
2021-12-20 18:59:32 +0530

    Create cities.component.ts

---
+++ Angular/Task-Day1/Task1/cities.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cities',
+  templateUrl: './cities.component.html',
+  styleUrls: ['./cities.component.css']
+})
+export class CitiesComponent  {
+
+  //city:string[] = ["Chennai", "Hyderabad", "Mumbai","Chennai"];
+  array:string[] = ["Angular","React","Node JS","Express"];
+
+}
