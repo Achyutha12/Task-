9b611519346e2b044ecf0ffb124fcbec91ffb5b2
Achyutha12 <96127510+Achyutha12@users.noreply.github.com>
2021-12-23 14:40:00 +0530

    Create product.component.ts

---
+++ Angular/Task-Day3/Task2/product.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { product } from '../models/product';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css']
+})
+export class ProductComponent  {
+
+  public mode:string = "";
+  public produArray:product[] = [
+
+    {pid : 10, pname : "i Phone", price : 60000, category : "Mobile", qty : 4},
+    {pid : 11, pname : "i Pad", price : 45000, category : "Tab", qty : 3},
+    {pid : 12, pname : "Samsung", price : 40000, category : "Mobile", qty : 6},
+    {pid : 13, pname : "RedMi", price : 20000, category : "Mobile", qty : 4},
+    {pid : 14, pname : "Lenovo", price : 60000, category : "Laptop", qty : 4},
+
+  ];
+
+  
+  
+}
